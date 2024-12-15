541ecafa84f07724aaea1bb335a2e3645be96bc7
ryusei-azuma03 <giantkilling03@gmail.com>
2024-12-15 10:50:10 +0900

    12151050

---
+++ connect.py
@@ -0,0 +1,44 @@
+import platform
+from sqlalchemy import create_engine
+import os
+import tempfile
+from dotenv import load_dotenv
+
+# 環境変数をロード
+load_dotenv()
+CONNECT = os.getenv("CONNECT")
+DATABASE_URL = os.getenv("DATABASE_URL")
+pem_content = os.getenv("SSL_CA_CERT")
+
+if CONNECT == "local":
+    print("===Connect to LocalDB===")
+    # ローカル環境でのデータベース接続設定
+    engine = create_engine(os.getenv('DB'), echo=True)
+else:
+    print("===Connect to AzureDB===")
+    # SSL証明書内容の確認と処理
+    if pem_content is None:
+        raise ValueError("SSL_CA_CERT is not set in environment variables.")
+    
+    # 改行とエスケープ文字の修正
+    pem_content = pem_content.replace("\\n", "\n").replace("\\", "")
+
+    # 一時ファイル作成
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=".pem") as temp_pem:
+        temp_pem.write(pem_content)
+        temp_pem_path = temp_pem.name
+
+    # デバッグ用に一時証明書の内容を表示
+    with open(temp_pem_path, "r") as temp_pem:
+        print("=====Temporary certificate file content:=====")
+        print(temp_pem.read())
+
+    # データベース接続設定
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={
+            "ssl": {
+                "ca": temp_pem_path
+            }
+        }
+    )
